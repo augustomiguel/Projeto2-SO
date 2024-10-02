23f3036a8613402409d6f4462e3f01fd0beea1e6
Vmp309 <vmp309@gmail.com>
2024-10-02 16:59:55 -0300

    lru

--- Projeto2/lru.py
+++ Projeto2/lru.py
@@ -14,18 +14,18 @@ class LRU:
         self.quadros = arq.quadros
         self.processos = arq.processos
         self.processosCopia = arq.processos
+        print(self.processosCopia)
         self.lista = [None]*self.quadros
         
         while self.processos:
             processoAtual = self.processos[0]
             self.processos.remove(processoAtual)
         
-            if processoAtual not in self.lista:
+            if processoAtual in self.lista:
+                self.indice += 1
+            else :
                 self.porNaLista(processoAtual)
-                
-                      
 
-        
     def porNaLista(self,processoAtual):
         
         if None in self.lista :
@@ -35,43 +35,44 @@ class LRU:
                     self.indice += 1 
                     break
         else:
-            
-            if self.primeiroInserido < self.quadros: 
-                
-                self.lista[self.primeiroInserido] = processoAtual
-                self.faltaDeQuadros += 1 
-                self.primeiroInserido += 1
-                print("falta de quadros",self.faltaDeQuadros)
-                
-            else:
-                self.primeiroInserido = 0
-                self.lista[self.primeiroInserido] = processoAtual  
-                self.faltaDeQuadros += 1 
-                print("falta de quadros tres ",self.faltaDeQuadros) 
+            self.buscarIndice(processoAtual)                
             
     def buscarIndice(self, processoAtual ):
         i = self.indice
         j = 0
-        k = 0
-        vetor[]
-        while i != -1: #and j <= self.quadros :
-            #if self.lista[j] == self.processosCopia[i]:
-            for j in range(self.quadros)
-                if self.processosCopia[i] == self.lista[j]
-                    vetor.append(j)
-            j += 1
-            
-            
+        vetor = []
+        print(self.processosCopia)
+        """
+        while i > -1: #and j <= self.quadros :
 
+            if len(vetor) == len(self.lista):
+                break
+
+            for j in range(len(self.lista) - 1 ):
+               
+                if self.processosCopia[i] == self.lista[j]:
+                    vetor.append(j)
+                    break
+                
+                j += 1                            
+                
             i -= 1 
+        self.inserir(vetor, processoAtual) """
+
+
+    def inserir(self, vetor , processoAtual):
+        self.lista[len(vetor)-1] = processoAtual
+        self.faltaDeQuadros += 1
+
+        
             
         
                 
                         
                 
-#arq = Arquivo()
-#arq.extrairProcessos("processos.txt")       
+arq = Arquivo()
+arq.lerArquivo("processos.txt")       
 fi=LRU()
-fi.lru()
+fi.lru(arq)
 #print("falta de quadros",fi.faltaDeQuadros) 
 #print(fi.faltaDeQuadros)
